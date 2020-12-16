e443ac76c8eb9f62eb5b5dc3aad6ef822fc765ff
berthornhill <64211468+berthornhill@users.noreply.github.com>
2020-12-16 14:17:15 -0500

    Completed phase1, Arrays

--- intro_to_javascript/phase_1_arrays.js
+++ intro_to_javascript/phase_1_arrays.js
@@ -8,4 +8,29 @@ Array.prototype.myUniq = function(){
     return uniq;
 }
 
-console.log([1,2,3].myUniq())
+// console.log([1,2,3].myUniq())
+
+Array.prototype.myTwoSum = function() {
+    let twoSum = [];
+    for (let i = 0; i < this.length; i++) {
+        for (let j = 0; j < this.length; j++) {
+            if ((i != j) && (this[i] + this[j] === 0)) {
+                twoSum.push([i, j]);
+            }
+        }
+    }
+    return twoSum;
+};
+
+Array.prototype.transpose = function() {
+    let transposed = [];
+    for (let col = 0; col < this[0].length; col++) {
+        let newRow = [];
+        for (let row = 0; row < this.length; row++) {
+            newRow.push(this[row][col]);
+        }
+        transposed.push(newRow);
+    }
+    return transposed;
+}
+// [1,2,3],[4,5,6]
