5401886b50d10c657df1efe5e33ebc344fb5b26e
Stephen <syangrea@gmail.com>
2020-12-16 16:34:18 -0500

    complete fib

--- intro_to_javascript/phase_4_recursion.js
+++ intro_to_javascript/phase_4_recursion.js
@@ -8,3 +8,36 @@ function range(start, end) {
         return prevArr;
     }
 }
+
+function sumRec(arr){
+    if (arr.length === 0){
+        return 0;
+    }else{
+        return arr.shift() + sumRec(arr);
+    }
+}
+
+function exponent(base, exp){
+    if (exp === 0){
+        return 1;
+    }else{
+        return base * exponent(base, exp - 1);
+    }
+    
+}
+
+function fibonacci(num){
+    if (num === 1) {
+        return [1];
+    } else if( num === 0){
+        return [];
+    } else if (num === 2){
+        return [1,1];
+    
+    }else{
+        let arr = fibonacci(num - 1);
+        return arr.concat(arr[arr.length-1] + arr[arr.length-2]);
+    }
+}
+
+
